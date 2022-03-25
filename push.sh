5ac64206e05b1d173af011ccf8920f45b26dcf4e
stringmanolo <manuelvarelacaldas@gmail.com>
2022-03-25 12:22:33 +0100

    Rm history from /root in push script

--- push.sh
+++ push.sh
@@ -4,7 +4,11 @@ if [[ -d soscwfs  ]]; then
   rm soscwfs/home/.ash_history 
   rm soscwfs/home/.bash_history
   rm soscwfs/home/.viminfo
+  rm soscwfs/root/.ash_history
+  rm soscwfs/root/.bash_history
+  rm soscwfs/root/.viminfo
   git add --all && git commit -m "$1" && git push
 else
   echo "Missing soscwfs folder!";
 fi
+
--- soscwfs/root/.bash_history
+++
@@ -1,2 +0,0 @@
-echi djdjd
-exit
